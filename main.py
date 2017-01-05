30f56300844e3f616e359afcd34d19a45a5d0f68
kosuke miyoshi <miyoshi@narr.jp>
2017-01-06 08:44:14 +0900

    add periodical saving

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ initial_learning_rate = log_uniform(INITIAL_ALPHA_LOW,
 global_t = 0
 
 stop_requested = False
+terminate_reqested = False
 
 action_size = Environment.get_action_size()
 global_network = UnrealModel(action_size, -1, device)
@@ -89,13 +90,63 @@ if checkpoint and checkpoint.model_checkpoint_path:
   wall_t_fname = CHECKPOINT_DIR + '/' + 'wall_t.' + str(global_t)
   with open(wall_t_fname, 'r') as f:
     wall_t = float(f.read())
+
+  next_save_steps = (global_t + SAVE_INTERVAL_STEP) // SAVE_INTERVAL_STEP * SAVE_INTERVAL_STEP
+    
 else:
   print("Could not find old checkpoint")
   # set wall time
   wall_t = 0.0
 
+  next_save_steps = SAVE_INTERVAL_STEP
+
+  
+def save(current_global_step):
+  """ Save checkpoint. 
+  
+  Called from therad-0.
+  """
+  global next_save_steps
+  global train_threads
+  global trainers
+  global saver
+  global stop_requested
+
+  stop_requested = True
+
+  # Wait for all other threads to stop
+  for (i, t) in enumerate(train_threads):
+    if i != 0:
+      t.join()
+
+  # Save
+  if not os.path.exists(CHECKPOINT_DIR):
+    os.mkdir(CHECKPOINT_DIR)
+
+  # Write wall time
+  wall_t = time.time() - start_time
+  wall_t_fname = CHECKPOINT_DIR + '/' + 'wall_t.' + str(global_t)
+  with open(wall_t_fname, 'w') as f:
+    f.write(str(wall_t))
+
+  print('Start saving.')
+  saver.save(sess, CHECKPOINT_DIR + '/' + 'checkpoint', global_step = global_t)
+  print('End saving.')  
+  
+  stop_requested = False
+  next_save_steps += SAVE_INTERVAL_STEP
+
+  # Restart other threads
+  for i in range(PARALLEL_SIZE):
+    if i != 0:
+      thread = threading.Thread(target=train_function, args=(i,))
+      train_threads[i] = thread
+      thread.start()
+
 
 def train_function(parallel_index):
+  """ Train each environment. """
+  
   global global_t
   
   trainer = trainers[parallel_index]
@@ -106,8 +157,13 @@ def train_function(parallel_index):
   while True:
     if stop_requested:
       break
+    if terminate_reqested:
+      break
     if global_t > MAX_TIME_STEP:
       break
+    if parallel_index == 0 and global_t > next_save_steps:
+      # Save checkpoint
+      save(global_t)
 
     diff_global_t = trainer.process(sess, global_t, summary_writer,
                                     summary_op, score_input)
@@ -115,10 +171,10 @@ def train_function(parallel_index):
     
     
 def signal_handler(signal, frame):
-  global stop_requested
+  global terminate_reqested
   print('You pressed Ctrl+C!')
-  stop_requested = True
-  
+  terminate_reqested = True
+
 train_threads = []
 for i in range(PARALLEL_SIZE):
   train_threads.append(threading.Thread(target=train_function, args=(i,)))
@@ -131,24 +187,5 @@ start_time = time.time() - wall_t
 for t in train_threads:
   t.start()
 
-# TODO: can't catch kill-signal to save in bazel environment.
 print('Press Ctrl+C to stop')
 signal.pause()
-
-print('Now saving data. Please wait')
-  
-for t in train_threads:
-  t.join()
-
-if not os.path.exists(CHECKPOINT_DIR):
-  os.mkdir(CHECKPOINT_DIR)
-
-# write wall time
-wall_t = time.time() - start_time
-wall_t_fname = CHECKPOINT_DIR + '/' + 'wall_t.' + str(global_t)
-with open(wall_t_fname, 'w') as f:
-  f.write(str(wall_t))
-
-saver.save(sess, CHECKPOINT_DIR + '/' + 'checkpoint', global_step = global_t)
-
-
