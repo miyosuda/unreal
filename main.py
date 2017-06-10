c3d0b27d28f4adcc11ce24ef54a4f8ffd3ed5592
Kosuke Miyoshi <miyosuda@gmail.com>
2017-06-11 01:37:49 +0900

    refactoring main

--- main.py
+++ main.py
@@ -25,174 +25,177 @@ def log_uniform(lo, hi, rate):
   v = log_lo * (1-rate) + log_hi * rate
   return math.exp(v)
 
-device = "/cpu:0"
-if USE_GPU:
-  device = "/gpu:0"
 
-initial_learning_rate = log_uniform(INITIAL_ALPHA_LOW,
-                                    INITIAL_ALPHA_HIGH,
-                                    INITIAL_ALPHA_LOG_RATE)
-
-global_t = 0
-
-stop_requested = False
-terminate_reqested = False
-
-action_size = Environment.get_action_size()
-global_network = UnrealModel(action_size, -1, device)
-
-trainers = []
-
-learning_rate_input = tf.placeholder("float")
-
-grad_applier = RMSPropApplier(learning_rate = learning_rate_input,
-                              decay = RMSP_ALPHA,
-                              momentum = 0.0,
-                              epsilon = RMSP_EPSILON,
-                              clip_norm = GRAD_NORM_CLIP,
-                              device = device)
-
-for i in range(PARALLEL_SIZE):
-  trainer = Trainer(i,
-                    global_network,
-                    initial_learning_rate,
-                    learning_rate_input,
-                    grad_applier,
-                    MAX_TIME_STEP,
-                    device = device)
-  trainers.append(trainer)
-
-# prepare session
-config = tf.ConfigProto(log_device_placement=False, allow_soft_placement=True)
-config.gpu_options.allow_growth = True
-sess = tf.Session(config=config)
-
-init = tf.global_variables_initializer()
-sess.run(init)
-
-# summary for tensorboard
-score_input = tf.placeholder(tf.int32)
-tf.summary.scalar("score", score_input)
-
-summary_op = tf.summary.merge_all()
-summary_writer = tf.summary.FileWriter(LOG_FILE, sess.graph)
-
-# init or load checkpoint with saver
-saver = tf.train.Saver(global_network.get_vars())
-checkpoint = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
-if checkpoint and checkpoint.model_checkpoint_path:
-  saver.restore(sess, checkpoint.model_checkpoint_path)
-  print("checkpoint loaded:", checkpoint.model_checkpoint_path)
-  tokens = checkpoint.model_checkpoint_path.split("-")
-  # set global step
-  global_t = int(tokens[1])
-  print(">>> global step set: ", global_t)
-  # set wall time
-  wall_t_fname = CHECKPOINT_DIR + '/' + 'wall_t.' + str(global_t)
-  with open(wall_t_fname, 'r') as f:
-    wall_t = float(f.read())
-
-  next_save_steps = (global_t + SAVE_INTERVAL_STEP) // SAVE_INTERVAL_STEP * SAVE_INTERVAL_STEP
+class Application(object):
+  def __init__(self):
+    pass
+  
+  def train_function(self, parallel_index, preparing):
+    """ Train each environment. """
     
-else:
-  print("Could not find old checkpoint")
-  # set wall time
-  wall_t = 0.0
-
-  next_save_steps = SAVE_INTERVAL_STEP
-
-  
-def save(current_global_step):
-  """ Save checkpoint. 
-  
-  Called from therad-0.
-  """
-  global next_save_steps
-  global train_threads
-  global trainers
-  global saver
-  global stop_requested
-
-  stop_requested = True
-
-  # Wait for all other threads to stop
-  for (i, t) in enumerate(train_threads):
-    if i != 0:
-      t.join()
-
-  # Save
-  if not os.path.exists(CHECKPOINT_DIR):
-    os.mkdir(CHECKPOINT_DIR)
-
-  # Write wall time
-  wall_t = time.time() - start_time
-  wall_t_fname = CHECKPOINT_DIR + '/' + 'wall_t.' + str(global_t)
-  with open(wall_t_fname, 'w') as f:
-    f.write(str(wall_t))
-
-  print('Start saving.')
-  saver.save(sess, CHECKPOINT_DIR + '/' + 'checkpoint', global_step = global_t)
-  print('End saving.')  
-  
-  stop_requested = False
-  next_save_steps += SAVE_INTERVAL_STEP
-
-  # Restart other threads
-  for i in range(PARALLEL_SIZE):
-    if i != 0:
-      thread = threading.Thread(target=train_function, args=(i,False))
-      train_threads[i] = thread
-      thread.start()
-
-
-def train_function(parallel_index, preparing):
-  """ Train each environment. """
-  
-  global global_t
-  
-  trainer = trainers[parallel_index]
-  if preparing:
-    trainer.prepare()
-  
-  # set start_time
-  start_time = time.time() - wall_t
-  trainer.set_start_time(start_time)
-
-  while True:
-    if stop_requested:
-      #trainer.stop()
-      break
-    if terminate_reqested:
-      trainer.stop()
-      break
-    if global_t > MAX_TIME_STEP:
-      trainer.stop()
-      break
-    if parallel_index == 0 and global_t > next_save_steps:
-      # Save checkpoint
-      save(global_t)
-
-    diff_global_t = trainer.process(sess, global_t, summary_writer,
-                                    summary_op, score_input)
-    global_t += diff_global_t
+    trainer = self.trainers[parallel_index]
+    if preparing:
+      trainer.prepare()
     
-    
-def signal_handler(signal, frame):
-  global terminate_reqested
-  print('You pressed Ctrl+C!')
-  terminate_reqested = True
-
-train_threads = []
-for i in range(PARALLEL_SIZE):
-  train_threads.append(threading.Thread(target=train_function, args=(i,True)))
+    # set start_time
+    trainer.set_start_time(self.start_time)
   
-signal.signal(signal.SIGINT, signal_handler)
+    while True:
+      if self.stop_requested:
+        break
+      if self.terminate_reqested:
+        trainer.stop()
+        break
+      if self.global_t > MAX_TIME_STEP:
+        trainer.stop()
+        break
+      if parallel_index == 0 and self.global_t > self.next_save_steps:
+        # Save checkpoint
+        self.save()
+  
+      diff_global_t = trainer.process(self.sess,
+                                      self.global_t,
+                                      self.summary_writer,
+                                      self.summary_op,
+                                      self.score_input)
+      self.global_t += diff_global_t
 
-# set start time
-start_time = time.time() - wall_t
+  def run(self):
+    device = "/cpu:0"
+    if USE_GPU:
+      device = "/gpu:0"
+    
+    initial_learning_rate = log_uniform(INITIAL_ALPHA_LOW,
+                                        INITIAL_ALPHA_HIGH,
+                                        INITIAL_ALPHA_LOG_RATE)
+    
+    self.global_t = 0
+    
+    self.stop_requested = False
+    self.terminate_reqested = False
+    
+    action_size = Environment.get_action_size()
+    
+    self.global_network = UnrealModel(action_size, -1, device)
+    self.trainers = []
+    
+    learning_rate_input = tf.placeholder("float")
+    
+    grad_applier = RMSPropApplier(learning_rate = learning_rate_input,
+                                  decay = RMSP_ALPHA,
+                                  momentum = 0.0,
+                                  epsilon = RMSP_EPSILON,
+                                  clip_norm = GRAD_NORM_CLIP,
+                                  device = device)
+    
+    for i in range(PARALLEL_SIZE):
+      trainer = Trainer(i,
+                        self.global_network,
+                        initial_learning_rate,
+                        learning_rate_input,
+                        grad_applier,
+                        MAX_TIME_STEP,
+                        device = device)
+      self.trainers.append(trainer)
+    
+    # prepare session
+    config = tf.ConfigProto(log_device_placement=False,
+                            allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+    self.sess = tf.Session(config=config)
+    
+    self.sess.run(tf.global_variables_initializer())
+    
+    # summary for tensorboard
+    self.score_input = tf.placeholder(tf.int32)
+    tf.summary.scalar("score", self.score_input)
+    
+    self.summary_op = tf.summary.merge_all()
+    self.summary_writer = tf.summary.FileWriter(LOG_FILE, self.sess.graph)
+    
+    # init or load checkpoint with saver
+    self.saver = tf.train.Saver(self.global_network.get_vars())
+    
+    checkpoint = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
+    if checkpoint and checkpoint.model_checkpoint_path:
+      self.saver.restore(self.sess, checkpoint.model_checkpoint_path)
+      print("checkpoint loaded:", checkpoint.model_checkpoint_path)
+      tokens = checkpoint.model_checkpoint_path.split("-")
+      # set global step
+      self.global_t = int(tokens[1])
+      print(">>> global step set: ", self.global_t)
+      # set wall time
+      wall_t_fname = CHECKPOINT_DIR + '/' + 'wall_t.' + str(self.global_t)
+      with open(wall_t_fname, 'r') as f:
+        self.wall_t = float(f.read())
+        self.next_save_steps = (self.global_t + SAVE_INTERVAL_STEP) // SAVE_INTERVAL_STEP * SAVE_INTERVAL_STEP
+        
+    else:
+      print("Could not find old checkpoint")
+      # set wall time
+      self.wall_t = 0.0
+      self.next_save_steps = SAVE_INTERVAL_STEP
+  
+    # run training threads
+    self.train_threads = []
+    for i in range(PARALLEL_SIZE):
+      self.train_threads.append(threading.Thread(target=self.train_function, args=(i,True)))
+      
+    signal.signal(signal.SIGINT, self.signal_handler)
+  
+    # set start time
+    self.start_time = time.time() - self.wall_t
+  
+    for t in self.train_threads:
+      t.start()
+  
+    print('Press Ctrl+C to stop')
+    signal.pause()
 
-for t in train_threads:
-  t.start()
+  def save(self):
+    """ Save checkpoint. 
+    Called from therad-0.
+    """
+    self.stop_requested = True
+  
+    # Wait for all other threads to stop
+    for (i, t) in enumerate(self.train_threads):
+      if i != 0:
+        t.join()
+  
+    # Save
+    if not os.path.exists(CHECKPOINT_DIR):
+      os.mkdir(CHECKPOINT_DIR)
+  
+    # Write wall time
+    wall_t = time.time() - self.start_time
+    wall_t_fname = CHECKPOINT_DIR + '/' + 'wall_t.' + str(self.global_t)
+    with open(wall_t_fname, 'w') as f:
+      f.write(str(wall_t))
+  
+    print('Start saving.')
+    self.saver.save(self.sess,
+                    CHECKPOINT_DIR + '/' + 'checkpoint',
+                    global_step = self.global_t)
+    print('End saving.')  
+    
+    self.stop_requested = False
+    self.next_save_steps += SAVE_INTERVAL_STEP
+  
+    # Restart other threads
+    for i in range(PARALLEL_SIZE):
+      if i != 0:
+        thread = threading.Thread(target=self.train_function, args=(i,False))
+        self.train_threads[i] = thread
+        thread.start()
+    
+  def signal_handler(self, signal, frame):
+    print('You pressed Ctrl+C!')
+    self.terminate_reqested = True
 
-print('Press Ctrl+C to stop')
-signal.pause()
+def main(argv):
+  app = Application()
+  app.run()
+
+if __name__ == '__main__':
+  tf.app.run()
